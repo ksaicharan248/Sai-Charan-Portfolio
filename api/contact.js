63dac3795af4cf8510bf765520b62e5ffa4dd519
saich <saicharanreddy141458@gmail.com>
2025-06-01 14:38:24 +0530

    mail handeling

--- api/contact.js
+++ api/contact.js
@@ -29,10 +29,23 @@ export default async function handler(req, res) {
             <p><strong>Message:</strong><br>${message}</p>
         `
     };
+    const Thanksmessage = {
+        from: process.env.GMAIL_USER,
+        to: email,
+        subject: "Thank You for Getting in Touch!",
+        html: `
+            <p>Dear ${name},</p>
+            <p>Thank you very much for reaching out to us. We truly appreciate you taking the time to contact us. Rest assured, we have received your message and will get back to you as soon as possible.</p>
+            <p>Warm regards,<br>K Sai Charan</p>
+        `
+    };
+
 
     try {
         await transporter.sendMail(mailOptions);
         res.status(200).json({ success: true });
+        await transporter.sendMail(Thanksmessage);
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Failed to send email." });
